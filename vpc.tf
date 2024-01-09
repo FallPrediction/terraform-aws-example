ffe71d112192721a25409262a7224d944da3bf8b
ElizabethHuang <lizne6z0@gmail.com>
2024-01-09 23:53:10 +0800

    Modify security group for gin.

--- vpc.tf
+++ vpc.tf
@@ -91,6 +91,13 @@ resource "aws_security_group" "public" {
   }
 
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -114,6 +121,13 @@ resource "aws_security_group" "public" {
   egress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
